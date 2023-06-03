a864b9793edebe8e1f48222560542efd11632776
romadebrian <roma_coll04@yahoo.com>
2023-06-03 14:25:33 +0700

    restucture

---
+++ src/pages/Home/index.js
@@ -0,0 +1,19 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const Home = () => {
+  return (
+    <View
+      style={{
+        height: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+      <Text>Home</Text>
+    </View>
+  );
+};
+
+export default Home;
+
+const styles = StyleSheet.create({});
